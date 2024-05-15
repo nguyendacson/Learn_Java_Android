97b8bed7e4e44d8e055b89c262685fad03c5f48d
dacson <dacson1822003@gmail.com>
2024-05-16 00:29:59 +0700

    update signup

--- app/src/main/java/com/example/layout_signin/MainActivity.java
+++ app/src/main/java/com/example/layout_signin/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.layout_signin;
 
+import static com.example.layout_signin.R.*;
+
+import android.annotation.SuppressLint;
+import android.app.slice.SliceItem;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,10 +25,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
     EditText textTaiKhoan,textMatKhau;
     Button buttonLogin,buttonSignup;
-    TextView textmesses;
-
+    TextView forgotpassswork,textor;
+    ProgressBar Barleft,Barright;
+    ImageButton showPasswork;
+    boolean isPasswordVisible = false;
+    private void mapping(){
+        textTaiKhoan = (EditText) findViewById(R.id.TaiKhoan);
+        textMatKhau = (EditText) findViewById(R.id.MatKhau);
+        buttonLogin = (Button) findViewById(R.id.Login);
+        buttonSignup = (Button)findViewById(R.id.Signup);
+        textor = (TextView) findViewById(R.id.textViewor);
+        showPasswork = (ImageButton) findViewById(R.id.buttonShowPasswork);
+        forgotpassswork = (TextView) findViewById(R.id.forgotPaswork);
+        Barleft = (ProgressBar) findViewById(R.id.progressBarleft);
+        Barright = (ProgressBar) findViewById(R.id.progressBarright);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,36 +54,47 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        mapping();
 
-        textTaiKhoan = (EditText) findViewById(R.id.TaiKhoan);
-        textMatKhau = (EditText) findViewById(R.id.MatKhau);
-        buttonLogin = (Button) findViewById(R.id.Login);
-        textmesses = (TextView) findViewById(R.id.messes);
 
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(textTaiKhoan.getText().toString().equals("nguyendacson")&&
-                textMatKhau.getText().toString().equals("1822003")){
-                    textmesses.setVisibility(View.VISIBLE);
-                    textmesses.setTextColor(Color.parseColor("#FF4500"));
-                    textmesses.setText("DONE");
-                }
-                else {
-                    textmesses.setVisibility(View.VISIBLE);
-                    textmesses.setText("Fail");
-                }
+        buttonLogin.setOnClickListener(view -> {
+            if(textTaiKhoan.getText().toString().equals("nguyendacson")&&
+            textMatKhau.getText().toString().equals("1822003")){
+
+                forgotpassswork.setVisibility(View.INVISIBLE);
+                Barleft.setVisibility(View.INVISIBLE);
+                Barright.setVisibility(View.INVISIBLE);
+                textor.setVisibility(View.INVISIBLE);
+                String noiDung = "Done";
+                Toast.makeText(MainActivity.this,noiDung,Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(MainActivity.this,Sign_up.class);
+                startActivity(intent);
+            }
+            else {
+                String noiDung = "Fail";
+                Toast.makeText(MainActivity.this,noiDung,Toast.LENGTH_LONG).show();
             }
         });
 
-        textTaiKhoan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(!textmesses.getText().toString().isEmpty()){
-                    textmesses.setText("");
-                }
-            }
+        buttonSignup.setOnClickListener(v -> {
+            Intent myIntent = new Intent(MainActivity.this, Sign_up.class);
+            startActivity(myIntent);
         });
 
+        showPasswork.setOnClickListener(v -> {
+            if (isPasswordVisible) {
+                textMatKhau.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                showPasswork.setImageResource(drawable.not_eye);
+                isPasswordVisible = false;
+            } else {
+                textMatKhau.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                showPasswork.setImageResource(drawable.eye);
+                isPasswordVisible = true;
+            }
+            textMatKhau.setSelection(textMatKhau.getText().length());
+        });
     }
 }
+
+
--- app/src/main/java/com/example/layout_signin/Sign_in.java
+++ app/src/main/java/com/example/layout_signin/Sign_in.java
@@ -1,8 +1,18 @@
 package com.example.layout_signin;
 
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,6 +20,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Sign_in extends AppCompatActivity {
 
+    ImageView imageView;
+    Button takeImage;
+
+
+    private void mapping(){
+        imageView = (ImageView) findViewById(R.id.imageView);
+        takeImage = (Button) findViewById(R.id.buttonTakeImage);
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +39,21 @@ public class Sign_in extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        mapping();
+        takeImage.setOnClickListener(v -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            activityResultLauncher.launch(intent);
+        });
     }
+    ActivityResultLauncher<Intent>activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null){
+                    Bundle bundle = result.getData().getExtras();
+                    Bitmap bitmap = (Bitmap) bundle.get("data");
+                    imageView.setImageBitmap(bitmap);
+                }
+            }
+    );
+
 }
